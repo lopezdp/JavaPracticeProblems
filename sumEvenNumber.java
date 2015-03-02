8adec44f4fb2f4ef9d947184015409cb353c9475
David Lopez <lopezdp@users.noreply.github.com>
2015-03-02 18:44:33 -0500

    Created sumEvenNumber.java
    Just a practice problem that uses a loop to add every second number from 2 - 100...

---
+++ sumEvenNumber.java
@@ -0,0 +1,26 @@
+/**
+ * David P. Lopez
+ * COP2800
+ * Bank Account Application
+ */
+
+/**
+ * 305.467.5719
+ * @author david.lopez016@mymdc.net
+ */
+public class SumEvenNumber {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int sum = 0;
+        for(int x = 2; x <= 100; x = x+2)
+        {
+            sum += x;
+        }
+        
+        System.out.println("The total sum is: " + sum);
+    }
+}
