9bb62cbb967b463bf0b218528e44036798b8c9c1
David Lopez <lopezdp@users.noreply.github.com>
2015-03-23 11:17:08 -0400

    Created ResControl.java
    Program that use the GroupSeating.java Class in the oysterBar Package to model a reservation system for a restaurant that can only seat a maximum of 100 guests...

---
+++ ResControl.java
@@ -0,0 +1,127 @@
+/**
+ * David P. Lopez
+ * COP2800 Test-2
+ * Programming Assignment
+ */
+package oysterBar;
+import java.util.Scanner;
+
+/**
+ * 305.467.5719
+ * @author david.lopez016@mymdc.net
+ */
+public class ResControl {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        boolean closed = false;
+        boolean t1 = true;
+        int menuSelect;
+        int numGuest;
+        Scanner userInput;
+        userInput = new Scanner(System.in);
+        
+        GroupSeating reservationSys = new GroupSeating();
+        
+        while(t1)
+        {
+            menuSelect = reservationSys.programMenu(closed);
+
+            if(menuSelect == 1)
+            {
+                boolean t = true;            
+                while(t)
+                {
+                    System.out.print ("Enter # of Guests in Group: ");
+                    if(userInput.hasNextInt())
+                    {
+                        numGuest = userInput.nextInt();
+                        if(numGuest > 0)
+                        {
+                            reservationSys.setGrSiz(numGuest);
+                            
+                            if((reservationSys.getGrSiz() + reservationSys.getBarSiz()) >= 100)
+                            {
+                                System.out.print ("Enter Q to QUIT the Program & Close: ");
+                                while(!userInput.hasNext("[Qq]"))
+                                {
+                                    System.out.print("PLEASE ENTER Q TO QUIT!!!");
+                                    userInput.next();
+                                }
+                                String exit = userInput.next();
+                                System.out.println("Thank you. The program is shutting down..." + exit);
+                                
+                                System.exit(0);
+
+                                break;
+                            }
+                            
+                            reservationSys.setBarStd(numGuest);
+                                                                               
+                            t = false;
+                        }
+                        else
+                        {
+                            System.out.println("Error!!! This is not a valid input!!!"
+                                    + "For security purposes we have recorded your IP address."
+                                    + "Please enter a valid input.");
+                        }                
+                    }
+                    else
+                    {
+                        System.out.println("Error!!! This is not a valid input!!!"
+                                + "For security purposes we have recorded your IP address."
+                                + "Please enter a valid input.");
+                        
+                        t = false;
+                    }
+                    reservationSys.setGrSiz(0);
+                }
+            }
+            
+            if(menuSelect == 2)
+            {
+                boolean t = true;            
+                while(t)
+                {
+                    System.out.print ("Enter # of Guests leaving Bar: ");
+                    if(userInput.hasNextInt())
+                    {
+                        numGuest = userInput.nextInt();
+                        if(numGuest > 0)
+                        {
+                            reservationSys.setGrSiz(-(numGuest));
+                            reservationSys.setBarStd(numGuest);
+                            reservationSys.setClosed();
+                            
+                            t = false;
+                        }
+                        else
+                        {
+                            System.out.println("Error!!! This is not a valid input!!!"
+                                    + "For security purposes we have recorded your IP address."
+                                    + "Please enter a valid input.");
+                        }                
+                    }
+                    else
+                    {
+                        System.out.println("Error!!! This is not a valid input!!!"
+                                + "For security purposes we have recorded your IP address."
+                                + "Please enter a valid input.");
+                    }
+                }
+            }
+            
+            System.out.println("Available Seating is: " + (100 - reservationSys.getBarSiz()));
+            
+            reservationSys.setGrSiz(0);
+        }
+        
+        System.out.println("You are at Max Capacity!!! "
+                + "The Program has TERMINATED & the BAR IS NOW CLOSED");
+    }   
+}
